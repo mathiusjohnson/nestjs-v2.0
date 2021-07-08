71dbb3cd885a7df6f84cff43835a6d927dc993d4
Mathius <devachievem@gmail.com>
2021-07-08 11:27:26 -0700

    add inputype and field to update user dto

--- src/users/dto/update-user.input.ts
+++ src/users/dto/update-user.input.ts
@@ -1,7 +1,11 @@
 import { CreateUserInput } from './create-user.input';
 import { PartialType } from '@nestjs/mapped-types';
-
+import { Field, InputType } from '@nestjs/graphql';
+@InputType()
 export class UpdateUserInput extends PartialType(CreateUserInput) {
+  @Field()
   id: string;
+
+  @Field()
   username: string;
 }
