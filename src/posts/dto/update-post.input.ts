d17da4d0ba3c151e93f8aa3f2ff479f6a6dd60e6
Mathius <devachievem@gmail.com>
2021-07-08 11:26:00 -0700

    add input type and field to update post dto

--- src/posts/dto/update-post.input.ts
+++ src/posts/dto/update-post.input.ts
@@ -1,7 +1,11 @@
 import { CreatePostInput } from './create-post.input';
 import { PartialType } from '@nestjs/mapped-types';
-
+import { Field, InputType } from '@nestjs/graphql';
+@InputType()
 export class UpdatePostInput extends PartialType(CreatePostInput) {
+  @Field()
   id: string;
+
+  @Field()
   text_body: string;
 }
