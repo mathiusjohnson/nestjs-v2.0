161bbee88bbfea118c372f069a85837290df45c3
Mathius <devachievem@gmail.com>
2021-07-08 11:27:46 -0700

    add posts to user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,10 +4,14 @@ import { UsersResolver } from './users.resolver';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersRepository } from './users.repository';
+import { PostsService } from 'src/posts/posts.service';
+import { PostsModule } from 'src/posts/posts.module';
+import { Post } from 'src/posts/entities/post.entity';
+// import { PostsRepository } from 'src/posts/posts.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersRepository])],
-  providers: [UsersResolver, UsersService],
+  imports: [TypeOrmModule.forFeature([UsersRepository, Post]), PostsModule],
+  providers: [UsersResolver, UsersService, PostsService],
   controllers: [UsersController],
 })
 export class UsersModule {}
