aad730b96274c1a1cba7c69e471b3b2eced57bf1
Mathius <devachievem@gmail.com>
2021-07-08 11:27:18 -0700

    add inputype and field to create user dto

--- src/users/dto/create-user.input.ts
+++ src/users/dto/create-user.input.ts
@@ -1,11 +1,14 @@
+import { Field, InputType } from '@nestjs/graphql';
 import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
-
+@InputType()
 export class CreateUserInput {
+  @Field()
   @IsString()
   @MinLength(4)
   @MaxLength(20)
   username: string;
 
+  @Field()
   @IsString()
   @MinLength(8)
   @MaxLength(32)
