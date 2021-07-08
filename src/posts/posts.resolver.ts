af3b592697bb448ab5ec082ddc073ff714ab200b
Mathius <devachievem@gmail.com>
2021-07-08 11:26:50 -0700

    refactor posts resolver

--- src/posts/posts.resolver.ts
+++ src/posts/posts.resolver.ts
@@ -2,32 +2,33 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { PostsService } from './posts.service';
 import { CreatePostInput } from './dto/create-post.input';
 import { UpdatePostInput } from './dto/update-post.input';
+import { Post } from './entities/post.entity';
 
 @Resolver('Post')
 export class PostsResolver {
   constructor(private readonly postsService: PostsService) {}
 
-  @Mutation('createPost')
+  @Mutation((returns) => Post, { name: 'createPost' })
   create(@Args('createPostInput') createPostInput: CreatePostInput) {
     return this.postsService.create(createPostInput);
   }
 
-  @Query('posts')
+  @Query((returns) => [Post], { name: 'getAllPosts' })
   findAll() {
     return this.postsService.findAll();
   }
 
-  @Query('post')
+  @Query((returns) => Post, { name: 'getPostById' })
   findOne(@Args('id') id: string) {
     return this.postsService.findOne(id);
   }
 
-  @Mutation('updatePost')
+  @Mutation((returns) => Post, { name: 'updatePost' })
   update(@Args('updatePostInput') updatePostInput: UpdatePostInput) {
     return this.postsService.updatePost(updatePostInput.id, updatePostInput);
   }
 
-  @Mutation('removePost')
+  @Mutation((returns) => Post, { name: 'removePost' })
   remove(@Args('id') id: string) {
     return this.postsService.remove(id);
   }
