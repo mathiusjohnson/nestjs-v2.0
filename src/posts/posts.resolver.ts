f7f96be05a57cd0c10c6ef7085c1113945a488c6
Mathius <devachievem@gmail.com>
2021-07-08 13:14:35 -0700

    refactor posts resolver

--- src/posts/posts.resolver.ts
+++ src/posts/posts.resolver.ts
@@ -1,10 +1,19 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { PostsService } from './posts.service';
 import { CreatePostInput } from './dto/create-post.input';
 import { UpdatePostInput } from './dto/update-post.input';
 import { Post } from './entities/post.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Logger } from '@nestjs/common';
 
-@Resolver('Post')
+@Resolver(() => Post)
 export class PostsResolver {
   constructor(private readonly postsService: PostsService) {}
 
@@ -23,6 +32,11 @@ export class PostsResolver {
     return this.postsService.findOne(id);
   }
 
+  // @Query(() => [Post], { name: 'getUserPosts' })
+  // findUserPosts(id: string) {
+  //   return this.postsService.findUserPosts(id);
+  // }
+
   @Mutation((returns) => Post, { name: 'updatePost' })
   update(@Args('updatePostInput') updatePostInput: UpdatePostInput) {
     return this.postsService.updatePost(updatePostInput.id, updatePostInput);
@@ -31,5 +45,13 @@ export class PostsResolver {
   @Mutation((returns) => Post, { name: 'removePost' })
   remove(@Args('id') id: string) {
     return this.postsService.remove(id);
+  }
+
+  @ResolveField((type) => User)
+  post(@Parent() post: Post) {
+    const logger = new Logger('get userposts in posts resolver');
+    const { posterId } = post;
+    logger.log(`id in user posts resolver: ${posterId}`);
+    return this.postsService.getUserPost(posterId);
   }
 }
