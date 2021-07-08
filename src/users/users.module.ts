4715e11c40be1e88ce9e1cc2bb64608c773b47b2
Mathius <devachievem@gmail.com>
2021-07-08 13:15:31 -0700

    refactor users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
-import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersRepository } from './users.repository';
-import { PostsService } from 'src/posts/posts.service';
-import { PostsModule } from 'src/posts/posts.module';
-import { Post } from 'src/posts/entities/post.entity';
+import { User } from './entities/user.entity';
 // import { PostsRepository } from 'src/posts/posts.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersRepository, Post]), PostsModule],
-  providers: [UsersResolver, UsersService, PostsService],
-  controllers: [UsersController],
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UsersResolver, UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
