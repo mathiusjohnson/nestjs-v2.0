21be79da1d907bfc77344b161bc9f902c2ae8a10
Mathius <devachievem@gmail.com>
2021-07-08 11:25:47 -0700

    add input type and field to create post dto

--- src/posts/dto/create-post.input.ts
+++ src/posts/dto/create-post.input.ts
@@ -1,9 +1,13 @@
+import { Field, InputType } from '@nestjs/graphql';
 import { IsString } from 'class-validator';
 
+@InputType()
 export class CreatePostInput {
+  @Field()
   @IsString()
   text_body: string;
 
+  @Field()
   @IsString()
   poster_id: string;
 }
