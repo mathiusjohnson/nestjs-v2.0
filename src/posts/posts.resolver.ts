4627bade86014a2cde38bb5aed356ba94b2127ff
Mathius <devachievem@gmail.com>
2021-07-09 13:30:15 -0700

    add authguard to create posts route

--- src/posts/posts.resolver.ts
+++ src/posts/posts.resolver.ts
@@ -11,14 +11,23 @@ import { CreatePostInput } from './dto/create-post.input';
 import { UpdatePostInput } from './dto/update-post.input';
 import { Post } from './entities/post.entity';
 import { User } from 'src/users/entities/user.entity';
-import { Logger } from '@nestjs/common';
+import { Logger, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth-guard';
+import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { Request } from 'express';
 
 @Resolver(() => Post)
 export class PostsResolver {
   constructor(private readonly postsService: PostsService) {}
 
   @Mutation((returns) => Post, { name: 'createPost' })
-  create(@Args('createPostInput') createPostInput: CreatePostInput) {
+  @UseGuards(JwtAuthGuard)
+  create(
+    @Req() req: Request,
+    // @GetUser() user: User,
+    @Args('createPostInput') createPostInput: CreatePostInput,
+  ) {
     return this.postsService.create(createPostInput);
   }
 
